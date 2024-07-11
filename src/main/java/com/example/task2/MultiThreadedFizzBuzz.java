444b8f004a1dbcc30b2f9e5a15a8670919bfab5e
Maes-tr0 <nikita.opara.work@gmail.com>
2024-07-11 11:58:47 +0300

    Added synchronize in task2

--- src/main/java/com/example/task2/MultiThreadedFizzBuzz.java
+++ src/main/java/com/example/task2/MultiThreadedFizzBuzz.java
@@ -2,9 +2,12 @@ package com.example.task2;
 
 import java.util.concurrent.BlockingDeque;
 import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class MultiThreadedFizzBuzz implements Runnable {
+    private final static Object MONITOR_LOCKER = new Object();
     private static int n;
+    private final AtomicInteger current = new AtomicInteger(1);
     private static final BlockingDeque<String> queue = new LinkedBlockingDeque<>();
 
     public MultiThreadedFizzBuzz(int n) {
@@ -46,40 +49,63 @@ public class MultiThreadedFizzBuzz implements Runnable {
         }).start(); // number
     }
 
-    private static void fizz() throws InterruptedException{
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 != 0) {
+    private void fizz() throws InterruptedException {
+        while (true) {
+            synchronized (MONITOR_LOCKER) {
+                while (current.get() <= n && (current.get() % 3 != 0 || current.get() % 5 == 0)) {
+                    MONITOR_LOCKER.wait();
+                }
+                if (current.get() > n) break;
                 queue.put("fizz");
+                current.incrementAndGet();
+                MONITOR_LOCKER.notifyAll();
             }
         }
     }
 
-    private static void buzz() throws InterruptedException{
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 != 0 && i % 5 == 0) {
+    private void buzz() throws InterruptedException {
+        while (true) {
+            synchronized (MONITOR_LOCKER) {
+                while (current.get() <= n && (current.get() % 5 != 0 || current.get() % 3 == 0)) {
+                    MONITOR_LOCKER.wait();
+                }
+                if (current.get() > n) break;
                 queue.put("buzz");
+                current.incrementAndGet();
+                MONITOR_LOCKER.notifyAll();
             }
         }
     }
 
-    private static void fizzbuzz() throws InterruptedException {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 == 0 && i % 5 == 0) {
+    private void fizzbuzz() throws InterruptedException {
+        while (true) {
+            synchronized (MONITOR_LOCKER) {
+                while (current.get() <= n && (current.get() % 3 != 0 || current.get() % 5 != 0)) {
+                    MONITOR_LOCKER.wait();
+                }
+                if (current.get() > n) break;
                 queue.put("fizzbuzz");
+                current.incrementAndGet();
+                MONITOR_LOCKER.notifyAll();
             }
         }
     }
 
-    private static void number() throws InterruptedException {
-        for (int i = 1; i <= n; i++) {
-            if (i % 3 != 0 && i % 5 != 0) {
-                System.out.print(i);
-            } else {
-                System.out.print(queue.take());
+    private void number() throws InterruptedException {
+        while (true) {
+            synchronized (MONITOR_LOCKER) {
+                while (current.get() <= n && (current.get() % 3 == 0 || current.get() % 5 == 0)) {
+                    MONITOR_LOCKER.wait();
+                }
+                if (current.get() > n) break;
+                queue.put(String.valueOf(current.get()));
+                current.incrementAndGet();
+                MONITOR_LOCKER.notifyAll();
             }
-            if(i != n){
-                System.out.print(" ");
-            }
+        }
+
+        while (!queue.isEmpty()) {
+            System.out.print(queue.take() + " ");
         }
     }
 }
